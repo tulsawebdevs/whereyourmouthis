56d85b3adf75ceb27f641df991b2699c8135e89a
groovecoder <luke.crouch@gmail.com>
2012-02-12 09:08:57 -0600

    more api tweaks

--- wymi/inspections/api.py
+++ wymi/inspections/api.py
@@ -1,3 +1,4 @@
+from tastypie import fields
 from tastypie.resources import ModelResource
 from .models import Facility, Inspection, Violation
 
@@ -7,9 +8,11 @@ class FacilityResource(ModelResource):
         queryset = Facility.objects.all()
 
 class InspectionResource(ModelResource):
+    facility = fields.ForeignKey(FacilityResource, 'facility')
     class Meta:
         queryset =  Inspection.objects.all()
 
 class ViolationResource(ModelResource):
+    inspection = fields.ForeignKey(InspectionResource, 'inspection')
     class Meta:
         queryset = Violation.objects.all()
--- wymi/urls.py
+++ wymi/urls.py
@@ -2,18 +2,18 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from tastypie.api import Api
 from inspections.api import (FacilityResource, InspectionResource,
                              ViolationResource)
 
 
-facility_resource = FacilityResource()
-inspection_resource = InspectionResource()
-violation_resource = ViolationResource()
+v1_api = Api(api_name='v1')
+v1_api.register(FacilityResource())
+v1_api.register(InspectionResource())
+v1_api.register(ViolationResource())
 
 urlpatterns = patterns('',
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/', include(facility_resource.urls)),
-    url(r'^api/', include(inspection_resource.urls)),
-    url(r'^api/', include(violation_resource.urls)),
+    url(r'^api/', include(v1_api.urls)),
 )
