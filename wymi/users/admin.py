b704c7ecdbe9080bc04a121a7c39026b6aa39902
groovecoder <luke.crouch@gmail.com>
2012-02-27 21:48:00 -0600

    inline apikey admin

--- wymi/users/admin.py
+++ wymi/users/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
+from tastypie.admin import ApiKeyInline
 from tastypie.models import ApiAccess, ApiKey
 
 admin.site.register(ApiAccess)
 admin.site.register(ApiKey)
+
+class UserModelAdmin(UserAdmin):
+    inlines = [ApiKeyInline]
+
+admin.site.unregister(User)
+admin.site.register(User, UserModelAdmin)
