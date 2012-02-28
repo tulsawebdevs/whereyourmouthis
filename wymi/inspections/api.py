ca647f9578093a123d5c15229bfbf53ec4d7b59b
groovecoder <luke.crouch@gmail.com>
2012-02-27 22:35:52 -0600

    creator field for resources; api authentication and authorization

--- wymi/inspections/api.py
+++ wymi/inspections/api.py
@@ -1,18 +1,35 @@
 from tastypie import fields
+from tastypie.authentication import ApiKeyAuthentication
+from tastypie.authorization import Authorization
 from tastypie.resources import ModelResource
 from .models import Facility, Inspection, Violation
 
 
+class CreatorAuthorization(Authorization):
+    def apply_limits(self, request, object_list):
+        if request and hasattr(request, 'user'):
+            if request.user.is_superuser:
+                return object_list
+            else:
+                return object_list.filter(
+                                    creator__username=request.user.username)
+
 class FacilityResource(ModelResource):
     class Meta:
         queryset = Facility.objects.all()
+        authentication = ApiKeyAuthentication()
+        authorization = CreatorAuthorization()
 
 class InspectionResource(ModelResource):
     facility = fields.ForeignKey(FacilityResource, 'facility')
     class Meta:
         queryset =  Inspection.objects.all()
+        authentication = ApiKeyAuthentication()
+        authorization = CreatorAuthorization()
 
 class ViolationResource(ModelResource):
     inspection = fields.ForeignKey(InspectionResource, 'inspection')
     class Meta:
         queryset = Violation.objects.all()
+        authentication = ApiKeyAuthentication()
+        authorization = CreatorAuthorization()
--- wymi/inspections/models.py
+++ wymi/inspections/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -8,14 +9,17 @@ class Facility(models.Model):
     state = models.CharField(max_length=5, blank=True)
     zip_code = models.CharField(max_length=10, blank=True)
     type = models.CharField(max_length=255, blank=True)
+    creator = models.ForeignKey(User, blank=True, null=True)
 
 class Inspection(models.Model):
     facility = models.ForeignKey('Facility')
     date = models.DateField()
     score = models.IntegerField(blank=True)
     type = models.CharField(max_length=255, blank=True)
+    creator = models.ForeignKey(User, blank=True, null=True)
 
 class Violation(models.Model):
     inspection = models.ForeignKey('Inspection')
     type = models.CharField(max_length=255, blank=True)
     details = models.CharField(max_length=255, blank=True)
+    creator = models.ForeignKey(User, blank=True, null=True)
