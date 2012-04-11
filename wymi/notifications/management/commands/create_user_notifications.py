1be78391fe908b9faa0440455b1764171424cb80
groovecoder <luke.crouch@gmail.com>
2012-04-10 21:53:19 -0500

    more notification logic

--- wymi/notifications/management/commands/create_user_notifications.py
+++ wymi/notifications/management/commands/create_user_notifications.py
@@ -1,4 +1,6 @@
+import datetime
 import json
+from urllib import urlencode
 import requests
 
 from django.core.management.base import BaseCommand
@@ -11,15 +13,22 @@ class Command(BaseCommand):
     help = "Create notifications for users."
 
     def handle(self, *args, **kwargs):
+        # TODO: refactor into a FBQL query like
+        # /fql?q=SELECT uid from checkin where author_uid in [x for x in usa.objects.all()['id']] and timestamp > 5 mins ago
         for usa in UserSocialAuth.objects.all():
             u = usa.user
             print "User %s" % u
             print "  facebook id: %s" % usa.uid
             print "  access token: %s" % usa.extra_data['access_token']
-            checkins_resp = requests.get('%s/%s/checkins?access_token=%s' %
-                (FACEBOOK_GRAPH_HOST, usa.uid, usa.extra_data['access_token']))
+            urlparams_dict = {
+                'access_token': usa.extra_data['access_token'],
+                'since': datetime.datetime.today() - datetime.timedelta(minutes=-5),
+            }
+            checkins_resp = requests.get('%s/%s/checkins?%s' %
+                (FACEBOOK_GRAPH_HOST, usa.uid, urlencode(urlparams_dict)))
             checkins = json.loads(checkins_resp.content)
-            print checkins['data'][0]
+            if checkins['data']:
+                print checkins['data'][0]
             # look for facility matching check-in
             # check facility's latest inspection score
             # if bad score, send notification
