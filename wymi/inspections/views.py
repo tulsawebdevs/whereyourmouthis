5492e7904ca1aaf90f59e4ce65f4c353c8e5713c
groovecoder <luke.crouch@gmail.com>
2012-09-16 15:05:05 -0500

    finish up csv loading code

--- wymi/inspections/views.py
+++ wymi/inspections/views.py
@@ -1,7 +1,11 @@
+import StringIO
+import csv
+
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from inspections.forms import FacilityImportForm
+from inspections.models import Facility
 
 def import_facility(request):
     form = FacilityImportForm(request.POST, request.FILES)
@@ -21,7 +25,6 @@ def load(request):
     """Load documents from uploaded file."""
     form = FacilityImportForm
     if request.method == 'POST':
-
         # Accept the uploaded document data.
         file_data = None
         form = FacilityImportForm(request.POST, request.FILES)
@@ -33,19 +36,33 @@ def load(request):
                 file_data = uploaded_file.read()
 
         if file_data:
-            pass
-            """
-            # Try to import the data, but report any error that occurs.
-            try:
-                counter = Document.objects.load_json(request.user, file_data)
-                user_msg = (_('%(obj_count)d object(s) loaded.') %
-                            {'obj_count': counter, })
-                messages.add_message(request, messages.INFO, user_msg)
-            except Exception, e:
-                err_msg = (_('Failed to import data: %(error)s') %
-                           {'error': '%s' % e, })
-                messages.add_message(request, messages.ERROR, err_msg)
-            """
+            f = StringIO.StringIO(file_data)
+            reader = csv.DictReader(f)
+            for row in reader:
+                try:
+                    data = {}
+                    data['name'] = row['name']
+                    data['address'] = row['address']
+                    data['latest_score'] = row['score']
+                    location = row['location']
+                    parts = location.split("\n")
+                    city_zip = parts[1].split(" ")
+                    data['city'] = city_zip[0]
+                    data['zip_code'] = city_zip[1]
+                    lat_lng = parts[2].strip('()').split(", ")
+                    if len(lat_lng) > 1:
+                        data['latitude'] = lat_lng[0]
+                        data['longitude'] = lat_lng[1]
+                    facilities = Facility.objects.filter(name=data['name'],
+                                                    address=data['address'])
+                    facility = Facility()
+                    if facilities:
+                        facility = facilities[0]
+                    for (key, value) in data.items():
+                        setattr(facility, key, value)
+                    facility.save()
+                except:
+                    pass
 
     context = {'import_file_form': form, }
     return render_to_response('admin/inspections/load_data_form.html',
