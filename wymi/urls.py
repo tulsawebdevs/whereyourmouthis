54a980248c44bc31e5ba4f3148af995729ada1a1
groovecoder <luke.crouch@gmail.com>
2012-10-13 12:22:16 -0500

    add region url using boundary service

--- wymi/urls.py
+++ wymi/urls.py
@@ -7,7 +7,7 @@ from tastypie.api import Api
 
 from inspections.api import (FacilityResource, InspectionResource,
                              ViolationResource)
-from inspections.views import load as iv_load
+from inspections.views import region, load as iv_load
 from home.views import HomeIndexView
 
 
@@ -26,5 +26,6 @@ urlpatterns = patterns('',
     url(r'^api/', include(v1_api.urls)),
     url(r'^$', HomeIndexView.as_view(), name="home"),
     url(r'^inspections/load/?$', iv_load, name='inspections_load'),
+    url(r'^region$', region, name='region'),
     url(r'^location/(\d+)/?', HomeIndexView.as_view(), name="home"),
 )
